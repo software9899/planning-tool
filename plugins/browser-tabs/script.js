e5af922488aede659615a5e7c8fb351afa49b525
Testtorial <software1212312121@gmail.com>
2025-12-27 18:54:13 +0700

    remove

--- plugins/browser-tabs/script.js
+++
@@ -1,3 +0,0 @@
-// Browser Tabs Plugin
-// This plugin integrates with the React Bookmarks page
-console.log('🔖 Browser Tabs Plugin loaded');
--- plugins/test-cases/script.js
+++
@@ -1,3 +0,0 @@
-// Test Cases Plugin
-// This plugin is integrated into the React app and doesn't need additional scripts
-console.log('Test Cases plugin loaded');
